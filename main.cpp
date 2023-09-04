7e6d90c6fafb9c6be31b98dc6d835c5686049a9e
aaronvg08 <A01800264@tec.mx>
2023-09-04 16:05:03 -0600

    Version 1.2
    Flecha direccional hecha. Ayuda para los botones en caso que se necesiten.
    
    Funcion para imprimir botones.

--- main.cpp
+++ main.cpp
@@ -5,23 +5,29 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    // Lista de botones
-    string buttons[10] = {"triggerL", "triggerR", "joystick1", "joystick2", "a", "b", "x", "y", "tabL", "tabR"};
-    cout << "Modo manejado!";
-
+void printButtons() {
     // Imprimir lista de botones
+    string buttons[12] = {"triggerl", "triggerr", "arrows", "a", "b", "x", "y", "tabL", "tabR"};
     cout << "\nLos botones disponibles son:\n";
     cout << "\n--------------------------------------";
-    for (string b : buttons) {
+    for (string b :  buttons) {
         cout << "\n" << b;
     }
     cout << "\n--------------------------------------\n";
+}
+
+int main() {
+    // Lista de botones
+    string buttons[12] = {"triggerl", "triggerr", "arrows", "a", "b", "x", "y", "tabL", "tabR"};
+    cout << "Modo manual!";
+
+    // Imprimir botones
+    printButtons();
 
     // Pedir boton
     string input;
     while (true) {
-        cout << "\nInserta tu boton: ";
+        cout << "\nInserta tu boton (o \"help\" para imprimir los botones): ";
         cin >> input;
         cin.clear();
         cin.ignore(1000, '\n');
@@ -29,10 +35,33 @@ int main() {
         // Transformar a minusculas
         transform(input.begin(), input.end(), input.begin(), ::tolower);
 
+        // Ver si es "help"
+        if (input == "help") {
+            printButtons();
+            continue;
+        }
+
         // Verificar si el boton existe
         if (std::find(std::begin(buttons), std::end(buttons), input) != end(buttons)) {
-            cout << "Tu boton si existe!!";
+            // El boton si existe
+            if (input == buttons[2]) {
+                // Es una flecha
+                string direction;
+                double time;
+                cout << "Inserta tu direccion deseada (up, down, left, right): ";
+                cin >> direction;
+                cout << "Tiempo deseado (segundos): ";
+                cin >> time;
+                cout << "Activando motor x, avanzando...";
+            } else if (input == buttons[0] || input == buttons[1]) {
+                // Es un gatillo
+            } else if (input == buttons[3] || input == buttons[4] || input == buttons[5] || input == buttons[6]) {
+                // Es un boton a b x y
+            } else if (input == buttons[7] || input == buttons[8]) {
+                // Es un tab
+            }
         } else {
+            // El boton no existe
             cout << "Tu boton no existe!!";
         }
     }
