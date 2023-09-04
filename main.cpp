0763f467c162b9e63d9941fe3f1d6d466a83f31f
aaronvg08 <A01800264@tec.mx>
2023-09-04 13:05:47 -0600

    Version 1.1
    Pedir input de boton y verificar que exista.

--- main.cpp
+++ main.cpp
@@ -6,14 +6,35 @@
 using namespace std;
 
 int main() {
+    // Lista de botones
     string buttons[10] = {"triggerL", "triggerR", "joystick1", "joystick2", "a", "b", "x", "y", "tabL", "tabR"};
     cout << "Modo manejado!";
+
+    // Imprimir lista de botones
     cout << "\nLos botones disponibles son:\n";
     cout << "\n--------------------------------------";
     for (string b : buttons) {
         cout << "\n" << b;
     }
     cout << "\n--------------------------------------\n";
-    cout << "Inserta tu boton:";
+
+    // Pedir boton
+    string input;
+    while (true) {
+        cout << "\nInserta tu boton: ";
+        cin >> input;
+        cin.clear();
+        cin.ignore(1000, '\n');
+
+        // Transformar a minusculas
+        transform(input.begin(), input.end(), input.begin(), ::tolower);
+
+        // Verificar si el boton existe
+        if (std::find(std::begin(buttons), std::end(buttons), input) != end(buttons)) {
+            cout << "Tu boton si existe!!";
+        } else {
+            cout << "Tu boton no existe!!";
+        }
+    }
     return 0;
 }
