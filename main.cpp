3ebbc259c954124b06733630dbf17920f9cc2f3d
aaronvg08 <A01800264@tec.mx>
2023-09-06 11:34:20 -0600

    Version 1.4
    - Modo autonomo añadido
         - NOTA: El modo autonomo aun no esta completado!! Es solo una demo
    
    - Remplazar "1000000" por una variable para no estar copiando y pegando todo el tiempo

--- main.cpp
+++ main.cpp
@@ -21,7 +21,81 @@ void printButtons() {
 int main() {
     // Lista de botones
     string buttons[5] = {"triggerl", "triggerr", "arrows", "tabl", "tabr"};
-    cout << "Modo manual!";
+    
+    // Microsegundos
+    int microsecond = 1000000;
+
+    // Tiempo para el autonomo
+    int aut_time = 15;
+
+    // Modo autonomo
+    cout << "Modo autonomo!";
+    int aut_input;
+
+    // Imprimir opciones autonomo
+    cout << "\n\nSelecciona una de las siguientes opciones:";
+    cout << "\n--------------------------------------";
+    cout << "\n- Quitar herramientas, x segundos (1)";
+    cout << "\n- Quitar llantas, x segundos (2)";
+    cout << "\n- Poner llantas, x segundos (3)";
+    cout << "\n--------------------------------------";
+
+    while (true) {
+        cout << "\n\nSelecciona una opcion (1, 2...): ";
+        cin >> aut_input;
+        cin.clear();
+        cin.ignore(1000, '\n');
+
+        if (aut_input >= 1 && aut_input < 4) {
+            // La opcion si existe
+            if (aut_input == 1) {
+                cout << "Quitando herramientas...";
+                std::cout << " " << std::flush;
+                
+                // Esperando el tiempo que tarda
+                usleep(5 * microsecond);
+                aut_time = aut_time - 5;
+            } else if (aut_input == 2) {
+                cout << "Quitando llantas...";
+                std::cout << " " << std::flush;
+
+                // Esperando el tiempo que tarda
+                usleep(5 * microsecond);
+                aut_time = aut_time - 5;
+            } else {
+                cout << "Colocando llantas...";
+                std::cout << " " << std::flush;
+
+                // Esperando el tiempo que tarda
+                usleep(5 * microsecond);
+                aut_time = aut_time - 5;
+            }
+            cout << "Listo!";
+            std::cout << " " << std::flush;
+            usleep(microsecond / 2);
+
+            // Mostrar tiempo autonomo restante
+            cout << "\n" << aut_time << " segundos restantes!";
+            std::cout << " " << std::flush;
+            usleep(1 * microsecond);
+
+            // Si se acabo el tiempo, se acaba el modo autonomo
+            if (aut_time <= 0) {
+                break;
+            }
+        } else {
+            cout << "Selecciona una opcion disponible!";
+        }
+    }
+    
+    cout << "\nFin del modo autonomo!";
+    std::cout << " " << std::flush;
+    usleep(3 * microsecond);
+
+    // Modo teleoperado
+    cout << "\n\nModo teleoperado!";
+    std::cout << " " << std::flush;
+    usleep(1 * microsecond);
 
     // Imprimir botones
     printButtons();
@@ -78,10 +152,10 @@ int main() {
                 std::cout << " " << std::flush;
 
                 // Espereando el tiempo
-                usleep(secs * 1000000);
+                usleep(secs * microsecond);
                 cout << "Listo!";
                 std::cout << " " << std::flush;
-                usleep(1000000 / 2);
+                usleep(microsecond / 2);
 
             } else if (input == buttons[0] || input == buttons[1]) {
                 // Es un gatillo
@@ -98,13 +172,13 @@ int main() {
                     cout << "Activando ruedas de chasis derecho.";
                     cout << "\nGirando 90 grados a la izquierda...";
                     std::cout << " " << std::flush;
-                    usleep(1000000);
+                    usleep(microsecond);
                 } else {
                     // Derecha
                     cout << "Activando ruedas de chasis izquierdo.";
                     cout << "\nGirando 90 grados a la derecha...";
                     std::cout << " " << std::flush;
-                    usleep(1000000);
+                    usleep(microsecond);
                 }
             }
         } else {
